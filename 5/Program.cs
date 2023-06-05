d90abb3d08433e183104a11d00d888d28f39e107
BaiazitovMR <133628617+BaiazitovMR@users.noreply.github.com>
2023-06-05 22:11:41 +0500

    dz

---
+++ 5/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 36: Задайте одномерный массив
+// , заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+ int [] array = new int [4];
+Console.Write("[");
+
+ for(int i = 0; i < array.Length; i++)
+ {
+     array[i] = new Random().Next(1,100);
+     Console.Write(array[i]+" ");
+ }
+ Console.Write("]");
+ int sum = 0;
+ for (int i = 0; i < array.Length; i+=2)
+      sum = sum + array[i];
+ Console.Write($"сумма элементов, стоящих на нечётных позициях = {sum}");
---
+++ 6/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 38: Задайте массив int чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+// [3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76
+
+ int [] array = new int [5];
+ Console.Write("[");
+
+ for(int i = 0; i < array.Length; i++)
+ {
+     array[i] = new Random().Next(1,100);
+     Console.Write(array[i]+" ");
+ }
+ Console.Write("]");
+
+ int min = array.Min();
+ int max = array.Max();
+
+ Console.Write($"Max = {max}, Min = {min}");
+ Console.Write($" Разница между Max и Min = {max - min}");
